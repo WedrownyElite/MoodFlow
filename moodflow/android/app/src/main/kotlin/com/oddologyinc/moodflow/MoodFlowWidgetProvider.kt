a4b4f08b70fe405785ac2944dcabaf7c599a3bd8
WedrownyElite <chris.sliwinski@gmail.com>
2025-09-25 12:21:31 -0600

    Update MoodFlowWidgetProvider.kt

--- moodflow/android/app/src/main/kotlin/com/oddologyinc/moodflow/MoodFlowWidgetProvider.kt
+++ moodflow/android/app/src/main/kotlin/com/oddologyinc/moodflow/MoodFlowWidgetProvider.kt
@@ -60,7 +60,7 @@ class MoodFlowWidgetProvider : AppWidgetProvider() {
             
             // Set selection state and alpha
             if (isSelected) {
-                views.setInt(moodButtonIds[i], "setBackgroundResource", R.drawable.enhanced_mood_button_selected)
+                views.setInt(moodButtonIds[i], "setBackgroundResource", R.drawable.mood_button_selected)
                 views.setFloat(moodButtonIds[i], "setAlpha", 1.0f)
                 views.setBoolean(moodButtonIds[i], "setSelected", true)
             } else {
